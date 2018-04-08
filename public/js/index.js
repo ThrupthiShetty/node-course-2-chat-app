ffa90d98be991f663df4f1e62c7a1c9717080c48
Thrupthi Shetty <Thrupthi.shetty@gmail.com>
2018-04-08 22:42:30 +0530

    Emit nemmessage on createmessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@ var socket = io();
 socket.on('connect',function (){
     console.log('connected to the server')
 
-    socket.emit('createMessage', {
-        from : 'trupti@exmaple.com',
-        createAt : 123
-    })
+   
 })
 socket.on('disconnect',function (){
     console.log('disconnected from the server')
--- server/server.js
+++ server/server.js
@@ -18,14 +18,15 @@ app.use(express.static(publicPath))
 io.on('connection',(socket) =>{
     console.log('New user connected')
 
-   socket.emit('newMessage', {
-       from : 'abc@exmaple.com',
-       to :'ac2@abc.com',
-       createAt : 213
-   })
+ 
 
    socket.on('createMessage', function (message) {
     console.log('create message',message)
+    io.emit('newMessage',{
+        from : message.from,
+        text : message.text,
+        createdAt : new Date().getTime()
+    })
 })
 
     socket.on('disconnect',()=>{
